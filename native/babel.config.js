16153002ad51a6ca58d273d55063586de13767da
William Boswell <williamboswell@Williams-MacBook-Pro.local>
2023-02-22 11:39:37 -0500

    completion

---
+++ native/babel.config.js
@@ -0,0 +1,15 @@
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ["babel-preset-expo"],
+    plugins: [
+      "@babel/plugin-proposal-export-namespace-from",
+      "react-native-reanimated/plugin",
+      "nativewind/babel",
+      ["module:react-native-dotenv", {
+        "safe": true
+      }],
+      require.resolve("expo-router/babel"),
+    ],
+  };
+};
